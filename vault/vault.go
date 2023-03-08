f65413535b9495ce2884dfb25e1fca44f1374d63
Daniel George Holz <dgholz@gmail.com>
2023-03-08 12:12:14 +0000

    Allow session tokens when MFA set in profile or source_profile

--- vault/vault.go
+++ vault/vault.go
@@ -309,16 +309,8 @@ func (t *tempCredsCreator) canUseGetSessionToken(c *ProfileConfig) (bool, string
 	}
 
 	if c.IsChained() {
-		if !c.ChainedFromProfile.HasMfaSerial() {
-			return false, fmt.Sprintf("profile '%s' has no MFA serial defined", c.ChainedFromProfile.ProfileName)
-		}
-
-		if !c.HasMfaSerial() && c.ChainedFromProfile.HasMfaSerial() {
-			return false, fmt.Sprintf("profile '%s' has no MFA serial defined", c.ProfileName)
-		}
-
-		if c.ChainedFromProfile.MfaSerial != c.MfaSerial {
-			return false, fmt.Sprintf("MFA serial doesn't match profile '%s'", c.ChainedFromProfile.ProfileName)
+		if !c.HasMfaSerial() && !c.ChainedFromProfile.HasMfaSerial() {
+			return false, fmt.Sprintf("no MFA serial defined in profile %s or source profile %s", c.ChainedFromProfile.ProfileName, c.ProfileName)
 		}
 
 		if c.ChainedFromProfile.AssumeRoleDuration > roleChainingMaximumDuration {
